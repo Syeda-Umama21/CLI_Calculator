72458c11822fa7230f02677cb4f6539896745833
Umama-Shah <165512076+Umama-Shah@users.noreply.github.com>
2024-06-01 21:51:00 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node 
+import chalk from "chalk";
+import inquirer from "inquirer";
+//user input  through inquirer
+const answers = await inquirer.prompt([
+    { message: chalk.green.bold("please Enter first number"), type: "number", name: "num1" },
+    { message: chalk.magenta.bold("please Enter second number"), type: "number", name: "num2" },
+    {
+        name: "operator",
+        type: "list",
+        message: chalk.yellow.bold("please select the operator"),
+        choices: ["+", "-", "*", "/", "**", "%"],
+    },
+]);
+//variable declaration
+let number1 = answers.num1;
+let number2 = answers.num2;
+let opt = answers.operator;
+// Conditional statement if-Else
+if (answers.operator === "+") {
+    console.log(chalk.rgb(0, 255, 255).bold(`${number1} ${opt} ${number2} = ${number1 + number2}`));
+}
+else if (answers.operator === "-") {
+    console.log(chalk.rgb(204, 255, 153).bold(`${number1} ${opt} ${number2} = ${number1 - number2}`));
+}
+else if (answers.operator === "*") {
+    console.log(chalk.rgb(128, 255, 0).bold(`${number1} ${opt} ${number2} = ${number1 * number2}`));
+}
+else if (answers.operator === "/") {
+    console.log(chalk.rgb(255, 178, 102).bold(`${number1} ${opt} ${number2} = ${number1 / number2}`));
+}
+else if (answers.operator === "**") {
+    console.log(chalk.rgb(219, 182, 229).bold(`${number1} ${opt} ${number2} = ${number1 ** number2}`));
+}
+else if (answers.operator === "%") {
+    console.log(chalk.rgb(204, 245, 215).bold(`${number1} ${opt} ${number2} = ${number1 % number2}`));
+}
+else {
+    console.log(chalk.red.bold("please select valid operator"));
+}
